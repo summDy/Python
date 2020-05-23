caee35d4a2dfdba6aa4373060e4ed1c2d5efe01a
huiweiyong <307468262@qq.com>
2020-05-23 12:00:59 +0800

    练习

--- demo2/main.py
+++ demo2/main.py
@@ -1,11 +1,5 @@
-
-# Python2.x 导入方法
-# from Tkinter import *           # 导入 Tkinter 库
-# Python3.x 导入方法
-# from tkinter import * 
 import tkinter as tk
-# import tkMessageBox
-import math
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -13,6 +7,7 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.create_widgets()
+        pass
 
     def create_widgets(self):
         self.hi_there = tk.Button(self)
@@ -22,18 +17,13 @@ class Application(tk.Frame):
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
+
         self.quit.pack(side="bottom")
-        pass
 
     def say_hi(self):
         print("hi there, everyone!")
-        pass
 
-content =dir(math)
-print (content)
-    
-root = tk.Tk()    #顶层窗口的对象，用于容纳所有内容
-root.title('第一个例程')    #设置标题
+
+root = tk.Tk()
 app = Application(master=root)
-app.mainloop()   #写完窗口以后最后要有这一句才能显示这整个窗口
-# app.quit()    #退出窗口
+app.mainloop()
